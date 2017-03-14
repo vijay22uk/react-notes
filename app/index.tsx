85d2cdda5284c52e9348f7ab0a7a4982987bc9a2
vijay pandey <vpandey@merrillcorp.com>
2017-03-14 16:21:13 +0530

    static reactjs

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import * as ReactDom from 'react-dom'
+
+import { AppComponent } from './components/app'
+
+
+ReactDom.render(
+    <AppComponent />,
+    document.getElementById('app')
+)
